c0aa84efb507767c500a4b087e7e97ec9dd7f429
derekinspace <derekwolf.3r2ws@slmail.me>
2025-08-07 20:52:04 +0530

    mismatch fixed 2

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.gradle.api.file.Directory
 
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
