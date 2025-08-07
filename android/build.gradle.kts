115e7289baeec5ea8906c13e0b0f4c7a4807f523
derekinspace <derekwolf.3r2ws@slmail.me>
2025-08-07 22:46:55 +0530

    version miss 6

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,5 @@
 import org.gradle.api.file.Directory
 
-plugins {
-    // Apply only the application plugin at the root; library plugin should be applied only in library submodules if any.
-    id("com.android.application") version "8.7.3" apply false
-    // Kotlin Android plugin version aligned with classpath (2.1.0) to avoid conflicts
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-}
-
 allprojects {
     repositories {
         google()
